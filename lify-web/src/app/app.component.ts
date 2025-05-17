58eb82b891b280b8f29512e0a38dfa8d434d3d26
aguslorenzo <aguslorenzo21@gmail.com>
2025-05-17 10:22:40 -0300

    agregado componente microgreen-list

--- lify-web/src/app/app.component.ts
+++ lify-web/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MicrogreenListComponent } from './microgreen-list/microgreen-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MicrogreenListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ lify-web/src/app/microgreen-list/Microgreen.ts
@@ -0,0 +1,7 @@
+export interface Microgreen {
+    name: string;
+    image: string;
+    price: number;
+    stock: number;
+    clearance: boolean;
+}
