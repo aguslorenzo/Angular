473456e4c7fc91fac5f60ede8b6a29233f6a858c
aguslorenzo <aguslorenzo21@gmail.com>
2025-05-17 11:08:38 -0300

    agregada navbar el styling quedo rari

--- lify-web/src/app/app.component.ts
+++ lify-web/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MicrogreenListComponent } from './microgreen-list/microgreen-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MicrogreenListComponent],
+  imports: [RouterOutlet, MicrogreenListComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
