6e9e6f733a704a403c62a61603670c562c8315d4
aguslorenzo <aguslorenzo21@gmail.com>
2025-06-28 12:32:25 -0300

    eliminado de los archivos de testing

---
+++ lify-web/src/app/input-integer/input-integer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-integer',
+  imports: [],
+  templateUrl: './input-integer.component.html',
+  styleUrl: './input-integer.component.scss'
+})
+export class InputIntegerComponent {
+
+}
