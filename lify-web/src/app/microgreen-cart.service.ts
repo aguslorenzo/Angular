b20e97aef3e19d81d0c870f80200b140f50865f4
aguslorenzo <aguslorenzo21@gmail.com>
2025-06-29 18:32:40 -0300

    segunda parte de componentes

--- lify-web/src/app/microgreen-cart.service.ts
+++ lify-web/src/app/microgreen-cart.service.ts
@@ -1,25 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Microgreen } from './microgreen-list/Microgreen';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MicrogreenCartService {
 
-  cartList: Microgreen[] = [];
+  private _cartList: Microgreen[] = [];
+  cartList: BehaviorSubject<Microgreen[]> = new BehaviorSubject<Microgreen[]>([]);
 
   constructor() { }
 
   shopList: Microgreen[]= [];
 
   addToCart(microgreen: Microgreen){
-    let item: Microgreen | undefined= this.cartList.find(v1 => v1.name == microgreen.name);
+    let item: Microgreen | undefined= this._cartList.find(v1 => v1.name == microgreen.name);
     if(!item){
-      this.cartList.push({... microgreen}); //clona el objeto
+      this._cartList.push({... microgreen}); //clona el objeto
     }else {
       item.quantity += microgreen.quantity;
     }
-    console.log(this.cartList);
+    console.log(this._cartList);
+    this.cartList.next(this._cartList);
   };
 
   
--- lify-web/src/app/microgreen-cart/microgreen-cart.component.ts
+++ lify-web/src/app/microgreen-cart/microgreen-cart.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { MicrogreenCartService } from '../microgreen-cart.service';
+import { Microgreen } from '../microgreen-list/Microgreen';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-microgreen-cart',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './microgreen-cart.component.html',
   styleUrl: './microgreen-cart.component.scss'
 })
 export class MicrogreenCartComponent {
 
+  cartList$: Observable<Microgreen[]>;
+
+
 constructor(private cart: MicrogreenCartService) {
-  }
+  this.cartList$ = cart.cartList.asObservable();
+}
+
 }
